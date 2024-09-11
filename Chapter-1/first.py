810bb37feca4bf79e87283311077fa3f63ed5f5f
=Sagar <=>
2024-09-11 12:43:33 +0500

    Comments, Modules and PIP

---
+++ Chapter-1/first.py
@@ -0,0 +1,6 @@
+print("hello world")
+
+
+
+#  module are programs that are written by some one else and we need to install those modules using pip for using
+# pip (Basically a package manager to install module pip install moduleName)
---
+++ Chapter-1/module.py
@@ -0,0 +1,12 @@
+import pyjokes
+joke  = pyjokes.get_joke()
+
+
+print(joke)
+
+# print("Ignore this print while running code")  single line comment
+
+
+"""
+multi line comment in python
+"""
